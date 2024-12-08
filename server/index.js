63b4086b1ebc98bffaee52ea90bd5693fa449307
Abisheik <your_email@example.com>
2024-12-08 17:08:38 +0530

    index file changed

--- server/index.js
+++ server/index.js
@@ -8,61 +8,54 @@ import { userRoute } from "./routes/userRoute.js";
 import { ownerRoute } from "./routes/ownerRoute.js";
 import { testRoute } from "./routes/test.route.js";
 import { adminRoute } from "./routes/adminRoute.js";
-
-
-import axios from "axios";  
-
+import axios from "axios";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.CLIENT_URL;
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
- 
-app.get("/", (req, res) => {
-    console.log("Test ok")
-    console.log("Test ok")
-})
+
+app.get("/test", (req, res) => {
+  console.log("Test ok");
+  res.status(200).send("Test ok");
+});
 
 app.use("/api/admin", adminRoute);
 app.use("/api/agent", agentRoute);
 app.use("/api/owner", ownerRoute);
-app.use("/api/test", testRoute)
+app.use("/api/test", testRoute);
 app.use("/api/residency", residencyRoute);
 app.use("/api/user", userRoute);
 
-
-
-
-
 app.get("/api/nearby-places", async (req, res) => {
-    const { lat, lng, type } = req.query;
-    const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
-  
-    try {
-      const response = await axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json", {
+  const { lat, lng, type } = req.query;
+  const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+
+  try {
+    const response = await axios.get(
+      "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
+      {
         params: {
           location: `${lat},${lng}`,
           radius: 9000,
           type,
           key: API_KEY,
         },
-      });
-      res.json(response.data);
-    } catch (error) {
-
-        console.error('Error fetching data from Google Places API:', error.response?.data || error.message); // Logs full error details
-       console.log("run")
-        res.status(500).json({ error: "Failed to fetch data from Google Places API" });
-    }
-  });
-  
- 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error(
+      "Error fetching data from Google Places API:",
+      error.response?.data || error.message
+    );
+    res
+      .status(500)
+      .json({ error: "Failed to fetch data from Google Places API" });
+  }
 });
 
 export default app;
