65cf55fd758f471141545ecc546e890bde13103b
R.Abisheik <137792859+Abi-de-jo@users.noreply.github.com>
2024-12-10 11:33:49 +0530

    Update residencyRoute.js

--- server/routes/residencyRoute.js
+++ server/routes/residencyRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { acceptResidency, createResidency, getAllagentDrafts, getAllPublishedResidency, getAllDraftResidencies, getAllResidency, getResidency, ownerTrack, updateResidency,publishResidency, agentsAchieve, agentsAchievedResidency, Achieve, AchievedResidency, deleteResidency, rentByAgent, updateResidency, getAllResidencyForAdmin, updateResidencyDiscount } from "../controllers/residencyCntrl.js";
+import { acceptResidency, createResidency, getAllagentDrafts, getAllPublishedResidency, getAllDraftResidencies, getAllResidency, getResidency, ownerTrack, publishResidency, agentsAchieve, agentsAchievedResidency, Achieve, AchievedResidency, deleteResidency, rentByAgent, updateResidency, getAllResidencyForAdmin, updateResidencyDiscount, updateDate } from "../controllers/residencyCntrl.js";
 // import jwtCheck from "../config/auth0Config.js";
 const router = express.Router();
 
@@ -25,7 +25,9 @@ router.put("/publish/:id", publishResidency);
 
 router.delete("/delete/:id", deleteResidency);
 router.post("/rentByAgent", rentByAgent);
+
 router.post("/update/:id", updateDate);
+
 
 
 
